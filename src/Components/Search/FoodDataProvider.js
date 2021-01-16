f975f6ba37561613d30fa8d153d1352d602ea566
Tristanwyatt <tristan7306@gmail.com>
2021-01-16 17:24:39 -0500

    deploy

--- src/Components/Search/FoodDataProvider.js
+++ src/Components/Search/FoodDataProvider.js
@@ -28,7 +28,7 @@ export const FoodDataProvider = (props) => {
     }
 
     const getNutrition = (food) => {
-        fetch(`https://api.spoonacular.com/recipes/parseIngredients?apiKey=${apiKey}&ingredientList=${food}&includeNutrition=true`, {
+        fetch(`https://api.spoonacular.com/recipes/parseIngredients?apiKey=${apiKey}&ingredientList=${food}180g&includeNutrition=true`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
