a3b70ed1413c897a046735f41b7e4b56c2305fe5
Tristanwyatt <tristan7306@gmail.com>
2021-01-17 13:40:16 -0500

    clean-up

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import { MacroTracker } from './MacroTracker'
 import './index.css';
 import "antd/dist/antd.css";
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
-
 
 ReactDOM.render(
   <React.StrictMode>
